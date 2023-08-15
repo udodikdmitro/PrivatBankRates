ae4a8b3e1e265388ea20e74f9cf5049f9df2bf50
dima udodik <dima udodik>
2023-08-15 23:30:29 +0300

    Ready code for pull request.

--- src/main/java/com/example/resttemplate/objects/Rate.java
+++ src/main/java/com/example/resttemplate/objects/Rate.java
@@ -1,17 +1,21 @@
 package com.example.resttemplate.objects;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Rate {
 
     private String ccy;
-    private String base_ccy;
+
+    @JsonProperty("base_ccy")
+    private String baseCcy;
     private String buy;
     private String sale;
 
     public Rate() {}
 
-    public Rate(String ccy, String base_ccy, String buy, String sale) {
+    public Rate(String ccy, String baseCcy, String buy, String sale) {
         this.ccy = ccy;
-        this.base_ccy = base_ccy;
+        this.baseCcy = baseCcy;
         this.buy = buy;
         this.sale = sale;
     }
@@ -20,37 +24,13 @@ public class Rate {
         return ccy;
     }
 
-    public void setCcy(String ccy) {
-        this.ccy = ccy;
-    }
-
-    public String getBase_ccy() {
-        return base_ccy;
-    }
-
-    public void setBase_ccy(String base_ccy) {
-        this.base_ccy = base_ccy;
-    }
-
-    public String getBuy() {
-        return buy;
-    }
-
-    public void setBuy(String buy) {
-        this.buy = buy;
-    }
-
     public String getSale() {
         return sale;
     }
 
-    public void setSale(String sale) {
-        this.sale = sale;
-    }
-
     @Override
     public String toString() {
-        return ccy + " against " + base_ccy +
+        return ccy + " against " + baseCcy +
                 ":" + '\n' + "buy: " + buy + "," + '\n' +
                 "sale: " + sale;
     }
